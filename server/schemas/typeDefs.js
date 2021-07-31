7a4e61aa7c411ac4b9323a5891d99427a33514d6
Trushil Budhia <trushil.budhia@gmail.com>
2021-07-31 17:05:28 +0800

    Updated resolves.js and typeDef.js within the server folder and created queries.js and mutations.js within the client folder

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,7 +29,7 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    login(email: String!, password: String!): Auth
+    loginUser(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(bookId: ID!, authors: [String]!, description: String, title: String, image: String, link: String): User
     removeBook(bookId: ID!): User
