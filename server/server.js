65612d98f2884eeaf929d88876c2dbb0d4fd01c8
Trushil Budhia <trushil.budhia@gmail.com>
2021-08-01 19:09:35 +0800

    Updated package.json, server.js and API.js

--- server/server.js
+++ server/server.js
@@ -1,46 +1,40 @@
 // require('dotenv').config();
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs } = require('./schemas');
 const { resolvers } = require('./resolvers');
 const { authMiddleware } = require('./utils/auth');
 
 const db = require('./config/connection');
-// const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-(async function startApolloServer() {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// If we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
-  await server.start();
-
-  server.applyMiddleware({ app });
-
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-
-  // If we're in production, serve client/build as static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
-
-  // app.use(routes);
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`🌍 Now listening on localhost:${PORT}`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    });
-  });
-})();
+});
