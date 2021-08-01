21ae6ce5d1d8791b613f20de47bb48c0dc783d42
Trushil Budhia <trushil.budhia@gmail.com>
2021-08-01 19:11:24 +0800

    Updated auth.js and removed routes and controllers folder

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = 'secret4noeyes';
 const expiration = '2h';
 
 module.exports = {
